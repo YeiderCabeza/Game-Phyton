0ebc790677ca71c3bb3a4a54d9c13083ac011230
YeiderCabeza <ycabeza5@misena.edu.co>
2023-05-09 22:36:44 -0500

    animacion

--- space_game/app.py
+++ space_game/app.py
@@ -14,14 +14,23 @@ class navespacial(pygame.sprite.Sprite):
         # posicion de la nave 
         #crea un nuevo rect con el tamaño de la imagen
         self.rect = self.imagenave.get_rect()
+        # toma el centro de la imagen 
         self.rect.centerx = ancho/2
         self.rect.centery = alto-30
         
         self.listaDisparo= []
         self.vida = True
-        self.velocidad = 5
+        self.velocidad = 20
         # print (self.rect)
-        
+    
+    def movimientoderecha(self):
+         self.rect.right += self.velocidad 
+         self.movimiento()
+         
+    def movimientoizqierda(self):
+        self.rect.left -= self.velocidad
+        self.movimiento()
+     
     def movimiento(self):
         if self.vida == True:
             if self.rect.left <=0:
@@ -43,7 +52,7 @@ class navespacial(pygame.sprite.Sprite):
     def disparar(self,x,y):
         # print("disparo")
         # creamos un objeto y lo guardamos en la listadedisparos
-        miproyectil = proyectil(x,y)
+        miproyectil = proyectil(x,y)# enviamos las cordenadas a proyrctil
         self.listaDisparo.append(miproyectil)
     
     def dibujar(self,superficie):
@@ -64,22 +73,59 @@ class proyectil(pygame.sprite.Sprite):
         
     def dibujar(self, superficie):
         superficie.blit(self.imagenProyectil, self.rect)
+
+class invasor(pygame.sprite.Sprite):
+    def __init__(self,posx, posy):
+        pygame.sprite.Sprite.__init__(self)
         
+        self.imgMarcianoA = pygame.image.load('space_game/assets/marcianoA.jpg')
+        self.imgMarcianoB = pygame.image.load('space_game/assets/marcianoB.jpg')
+        self.listaEnemigos = [self.imgMarcianoA, self.imgMarcianoB]
+        self.posImagen = 0
+        
+        self.imagenInvasor = self.listaEnemigos[self.posImagen]
+        self.rect = self.imagenInvasor.get_rect()
+       
+        self.listadisparo = []
+        self.velocidad = 20
+        self.rect.top = posy
+        self.rect.left = posx
+        
+        self.tiempoCambio = 1 
+        
+    def dibujar(self, superficie):
+        self.imagenInvasor = self.listaEnemigos[self.posImagen]
+        superficie.blit(self.imagenInvasor, self.rect)
+          
+    def comportamiento (self, tiempo): 
+        # self.posImagen = int(tiempo % 2)
+        if self.tiempoCambio == tiempo:
+            self.posImagen += 1
+            self.tiempoCambio +=1
+            
+            if self.posImagen > len(self.listaEnemigos)-1:
+                self.posImagen = 0
        
 def SpaceGame():
     pygame.init()
     pantalla = pygame.display.set_mode((ancho,alto))
     pygame.display.set_caption("Space Game")
-    
     background= pygame.image.load("space_game/assets/Fondo.jpg")
+    
     jugador = navespacial()
-    demoproyectil = proyectil(ancho/2, alto-30)
+    enemigo = invasor(100,100)
+    # demoproyectil = proyectil(ancho/2, alto-30) se envia al init de protetil
     enjuego = True
+    reloj = pygame.time.Clock()
+    
+    
     
     while True:
         
-        jugador.movimiento()
-        demoproyectil.trayetoria()
+        reloj.tick(180)
+        tiempo = int(pygame.time.get_ticks()/1000)
+        # jugador.movimiento()
+        # demoproyectil.trayetoria()
         
         for evento in pygame.event.get():
             if evento.type == QUIT:
@@ -89,10 +135,10 @@ def SpaceGame():
             if enjuego== True:
                 if evento.type == pygame.KEYDOWN:
                     if evento.key == K_LEFT:
-                        jugador.rect.left -= jugador.velocidad
+                        jugador.movimientoizqierda()
                         
                     elif evento.key == K_RIGHT:
-                        jugador.rect.right += jugador.velocidad   
+                        jugador.movimientoderecha()  
                         
                     elif evento.key == K_s:
                         # jugador.disparar()
@@ -101,7 +147,10 @@ def SpaceGame():
                         jugador.disparar(x,y)
         
         pantalla.blit(background,(0,0))
+        enemigo.comportamiento(tiempo)# le enviamos el tiempo 
         jugador.dibujar(pantalla)
+        enemigo.dibujar(pantalla)
+        
         # demoproyectil.dibujar(pantalla)
         
         if len(jugador.listaDisparo)>0:
